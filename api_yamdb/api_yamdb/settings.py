edc61bb71262aa5c0a4e9ee3e75f17de1a060b95
Андрей Катышев <Andrey-Katyshev@mail.ru>
2022-12-13 12:13:26 +0300

    added secrets

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,11 +7,13 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '${{ secrets.SECRET_KEY }}'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+# ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = '${{ secrets.ALLOWED_HOSTS }}'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -64,12 +66,19 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='USER'),
-        'HOST': os.getenv('DB_HOST', default='USER'),
-        'PORT': os.getenv('DB_PORT', default='USER'),
+        'ENGINE': '${{ secrets.DB_ENGINE }}',
+        'NAME': '${{ secrets.DB_NAME }}',
+        'USER': '${{ secrets.POSTGRES_USER }}',
+        'PASSWORD': '${{ secrets.POSTGRES_PASSWORD }}',
+        'HOST': '${{ secrets.DB_HOST }}',
+        'PORT': '${{ secrets.DB_PORT }}',
+
+        # 'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        # 'NAME': os.getenv('DB_NAME', default='postgres'),
+        # 'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='USER'),
+        # 'HOST': os.getenv('DB_HOST', default='USER'),
+        # 'PORT': os.getenv('DB_PORT', default='USER'),
     }
 }
 
